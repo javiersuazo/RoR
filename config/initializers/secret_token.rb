62c72447311e1723a791c83710a28d4123c307e1
javier <javier@javier-HP-Compaq-6715b-KL428LA-ABM.(none)>
2011-07-23 20:08:01 -0600

    Tuto Java

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoR::Application.config.secret_token = '328bcaf8d5fd61dcd2ed17dbd70b0419ba303c5f48ec3edcfbabfe11af651e7004ef14d63046a831fc55540ee74d3893ddb4483150855dba263c7d287024e0b7'
